03f7f7d4bdc067b9f55a296f9aa7804cc31e5781
Sindt <czindt@gmail.com>
2017-09-15 23:53:08 +0200

    Update Assignment_2.py

--- Assignment_2/Assignment_2.py
+++ Assignment_2/Assignment_2.py
@@ -21,13 +21,13 @@ rows = table_body.find('table').find('tbody').find_all('tr')
 
 for row in rows:
     cols = row.find_all('td')
-    
+        #address,zip_code,price,sell_date,sell_type,price_per_sq_m,no_rooms,housing_type,size_in_sq_m,year_of_construction,price_change_in_pct
     address = cols[0].find('a')
         
     address_str = str(address).replace("<br/>", ">").split('>')
         # WORKS
     street_str = address_str[1]
-    zip_code = address_str[2].replace("</a", " ")
+    zip_code = address_str[2].replace("</a", "")
     
     try:
         price = int(cols[1].text)
@@ -36,9 +36,27 @@ for row in rows:
             price = cols[1].text
         except ValueError:
             price = 'ERROR'
-
-    decoded_row = (street_str, zip_code, price)
+                
+    cols_sell = str(cols[2]).replace("<br/>", ">").split('>')
+    
+    sell_date = cols_sell[2]
+    
+    sell_type = cols_sell[3].replace("</h5", "")
+    try:
+        price_per_sq_m = int(cols[3].text)
+    except ValueError:
+        price_per_sq_m = cols[3].text
+    
+    try:
+        no_rooms = int(cols[4].text)
+    except ValueError:
+        no_rooms = cols[4].text
+        
+        
+    decoded_row = (street_str, zip_code, price, sell_date,sell_type, price_per_sq_m, no_rooms)
     data.append(decoded_row)
     
-print(data[10])
+print(data[1])
+
+
 
