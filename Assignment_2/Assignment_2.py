828e518e726de1fb922fb9272622207fa39b81c0
Sindt <czindt@gmail.com>
2017-09-16 16:19:08 +0200

    Update Assignment_2.py

--- Assignment_2/Assignment_2.py
+++ Assignment_2/Assignment_2.py
@@ -7,56 +7,71 @@ import bs4
 import requests
 from IPython.display import IFrame
 
-url = 'http://138.197.184.35/boliga/1050-1549_1.html'
-IFrame(url, width=700, height=400)
-
-url = 'http://138.197.184.35/boliga/1050-1549_1.html'
-r = requests.get(url)
-soup = bs4.BeautifulSoup(r.content.decode('utf-8'), 'html5lib')
-
-data = []
-
-table_body = soup.find('body')
-rows = table_body.find('table').find('tbody').find_all('tr')
-
-for row in rows:
-    cols = row.find_all('td')
-        #address,zip_code,price,sell_date,sell_type,price_per_sq_m,no_rooms,housing_type,size_in_sq_m,year_of_construction,price_change_in_pct
-    address = cols[0].find('a')
-        
-    address_str = str(address).replace("<br/>", ">").split('>')
-        # WORKS
-    street_str = address_str[1]
-    zip_code = address_str[2].replace("</a", "")
+class Scraper:
     
-    try:
-        price = int(cols[1].text)
-    except ValueError:
+    def __init__(self, url):
+        self.url = url
+        
+    def parseToInt(text):
         try:
-            price = cols[1].text
+            return int(text)
         except ValueError:
-            price = 'ERROR'
+            return text    
+
+    def scrape(self):
+        data = []
+        
+        IFrame(self.url, width=700, height=400)
+        
+        r = requests.get(self.url)
+        soup = bs4.BeautifulSoup(r.content.decode('utf-8'), 'html5lib')
+
+        table_body = soup.find('body')
+        rows = table_body.find('table').find('tbody').find_all('tr')
+
+        for row in rows:
+            cols = row.find_all('td')
+       
+            address = cols[0].find('a')    
+            address_str = str(address).replace("<br/>", ">").split('>')
+            street_str = address_str[1]
+    
+            zip_code = address_str[2].replace("</a", "")
+    
+            price = parseToInt(cols[1].text.replace(".", ""))    
                 
-    cols_sell = str(cols[2]).replace("<br/>", ">").split('>')
+            cols_sell = str(cols[2]).replace("<br/>", ">").split('>')
     
-    sell_date = cols_sell[2]
+            sell_date = cols_sell[2]
     
-    sell_type = cols_sell[3].replace("</h5", "")
-    try:
-        price_per_sq_m = int(cols[3].text)
-    except ValueError:
-        price_per_sq_m = cols[3].text
+            sell_type = cols_sell[3].replace("</h5", "")
     
-    try:
-        no_rooms = int(cols[4].text)
-    except ValueError:
-        no_rooms = cols[4].text
+            price_per_sq_m = parseToInt(cols[3].text.replace(".",""))
+    
+            no_rooms = parseToInt(cols[4].text)
         
-        
-    decoded_row = (street_str, zip_code, price, sell_date,sell_type, price_per_sq_m, no_rooms)
-    data.append(decoded_row)
+            housing_type = cols[5].text
     
-print(data[1])
+            size_in_sq_m = parseToInt(cols[6].text)
+    
+            year_of_contruction = parseToInt(cols[7].text)   
+    
+            price_change_in_pct = cols[8].text.replace("\n", "").strip()
+    
+            decoded_row = (street_str, zip_code, price, sell_date,sell_type, price_per_sq_m, no_rooms,
+                           housing_type, size_in_sq_m, year_of_contruction, price_change_in_pct)
+            data.append(decoded_row)
+    
+        print(data[1])
+        print(data[10])
+        print(data[37])
+   
+    
+class main:
+    scraper = Scraper('http://138.197.184.35/boliga/2650_38.html')
+    
+    scraper.scrape()
+
 
 
 
